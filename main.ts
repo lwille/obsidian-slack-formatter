ae758c225fe401b94b7ba3f9527c717c7a7f1615
Leonhardt Wille <leonhardt.wille@camunda.com>
2021-05-12 17:24:42 +0200

    chore(formatting): formatted main.ts with prettier

--- main.ts
+++ main.ts
@@ -1,33 +1,42 @@
-import { App, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+  App,
+  MarkdownView,
+  Modal,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting
+} from "obsidian";
 
 interface SlackFormatterSettings {
-	mySetting: string;
+  mySetting: string;
 }
 
 const DEFAULT_SETTINGS: SlackFormatterSettings = {
-	mySetting: 'default'
-}
+  mySetting: "default"
+};
 
 export default class SlackFormatter extends Plugin {
-	settings: SlackFormatterSettings;
+  settings: SlackFormatterSettings;
 
-	async onload() {
-		console.log('loading Slack Formatter plugin');
+  async onload() {
+    console.log("loading Slack Formatter plugin");
 
-		this.addRibbonIcon('dice', 'Format and copy for Slack', () => {
-			const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+    this.addRibbonIcon("dice", "Format and copy for Slack", () => {
+      const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
 
-			if(activeView){
-				const text = activeView.editor.somethingSelected() ? activeView.editor.getSelection() : activeView.editor.getValue()
-				const formatted = [
-					// TODO: make replacements configurable
-					[/^## (.*)$/gm, "* $1 *"],
-					[/\[\[(.*)\]\]/gm, "$1"]
-				].reduce((s, args) => s.replace.apply(s, args), text)
-				navigator.clipboard.writeText(formatted)
-				new Notice('Formatted and copied current file!');
-			}
-
-		});
-	}
+      if (activeView) {
+        const text = activeView.editor.somethingSelected()
+          ? activeView.editor.getSelection()
+          : activeView.editor.getValue();
+        const formatted = [
+          // TODO: make replacements configurable
+          [/^## (.*)$/gm, "* $1 *"],
+          [/\[\[(.*)\]\]/gm, "$1"]
+        ].reduce((s, args) => s.replace.apply(s, args), text);
+        navigator.clipboard.writeText(formatted);
+        new Notice("Formatted and copied current file!");
+      }
+    });
+  }
 }
